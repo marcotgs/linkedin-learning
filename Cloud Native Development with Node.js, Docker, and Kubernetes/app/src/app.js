4bf764078c566726705c4d7e0e6565a5b50a7a03
Marco Gualberto <Marco_Gualberto@epam.com>
2020-03-01 18:40:57 +0100

    add zipkin package

--- Cloud Native Development with Node.js, Docker, and Kubernetes/app/src/app.js
+++ Cloud Native Development with Node.js, Docker, and Kubernetes/app/src/app.js
@@ -1,3 +1,8 @@
+const zipkin = require('appmetrics-zipkin')({
+  host: 'jaeger-collector.jaeger.svc.cluster.local',
+  port: 9411,
+  serviceName:'nodeserver'
+});
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
