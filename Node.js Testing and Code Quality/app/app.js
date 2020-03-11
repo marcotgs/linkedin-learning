6369ce5566f4058f154af7e0e3a640c1135cc28e
Marco Gualberto <Marco_Gualberto@epam.com>
2020-03-11 16:21:56 +0100

    add prettier config

--- Node.js Testing and Code Quality/app/app.js
+++ Node.js Testing and Code Quality/app/app.js
@@ -11,20 +11,9 @@ let reservations = require('./routes/reservations');
 
 const auth = require('./lib/middleware/auth.js');
 
-const configuration = {}
-
-
-
-
-
-
-
-
-
-
+const configuration = {};
 
 if (1) {
-
 }
 
 var app = express();
@@ -47,27 +36,27 @@ app.use(cookieParser());
 app.use('/', index);
 
 // Impenetrable security.
-app.use('/admin', auth('admin', 'admin'))
+app.use('/admin', auth('admin', 'admin'));
 app.use('/admin', admin);
 
 app.use('/reservations', reservations);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
